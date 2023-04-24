38e6bc98ea684b7bcaf65571790592a1fae5d2a0
AntonyIS <antonyshikubu@gmail.com>
2023-04-24 16:26:07 +0300

    Adding initial reactjs components

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+export default function Body (){
+    return(
+        <>
+            <div className="container">
+                <div className="Jumbotron">
+                    <p className="text text-center">
+                        Free, high quality, open source icon library with over 1,800 icons. Include them anyway you like—SVGs, SVG sprite, or web fonts. Use them with or without Bootstrap in any project.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function  Navbar(){
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container">
+                <span className="navbar-brand mb-0 h1">Antony Injila</span>
+            </div>
+        </nav>
+    )
+}
