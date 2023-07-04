c95dac11cbc1569f76dae7f4193fb8706831686c
AntonyIS <antonyshikubu@gmail.com>
2023-07-04 14:46:52 +0300

    MC

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Blogs from './Pages/Blogs.js'
-import Home from './Pages/Home.js'
-import Users from './Pages/Users.js'
-import Login from './Auth/Login.js'
-import Logout from './Auth/Logout.js'
-import Signup from './Auth/Signup.js'
-import Navbar from "./Home/Navbar.js";
+import Blogs from './Blogs.js'
+import Home from './Home.js'
+// import Users from './Users.js'
+// import Login from './Login.js'
+// import Logout from './Logout.js'
+// import Signup from './Auth/Signup.js'
+import Navbar from "./Navbar.js";
 
 
 export default function App(){
@@ -16,11 +16,11 @@ export default function App(){
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />}/>
-                <Route path="/blogs" element={<Blogs />}/>
+                {/* <Route path="/blogs" element={<Blogs />}/>
                 <Route path="/users" element={<Users />}/>
                 <Route path="/signup" element={<Signup />}/>
                 <Route path="/login" element={<Login />}/>
-                <Route path="/logout" element={<Logout />}/>
+                <Route path="/logout" element={<Logout />}/> */}
             </Routes>
             </BrowserRouter>
         </>
